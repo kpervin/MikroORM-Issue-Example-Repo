4e71aa7f2f1e720f1523a882bd631f6262bb30f4
Kristofer Pervin <7747148+kpervin@users.noreply.github.com>
2022-11-22 14:26:29 -0400

    Fixed comment

--- test/setup-jest.global.ts
+++ test/setup-jest.global.ts
@@ -4,8 +4,6 @@ import { MikroORM } from "@mikro-orm/core";
 module.exports = async () => {
   /**
    * This is to ensure that MikroORM cache is built before testing.
-   * This way we can also have a cleaner `await` call instead of having to do
-   * the above.
    */
   await MikroORM.init(config);
 };
