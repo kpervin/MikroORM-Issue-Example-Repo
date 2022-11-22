debbfb27322209bb26d092f3c4f7e1f9aa3a8c21
Kristofer Pervin <7747148+kpervin@users.noreply.github.com>
2022-11-22 16:25:36 -0400

    Changed the test to not use NestJS at all and narrow down to MikroORM and Jest runtime.

--- src/database/foo/foo.service.spec.ts
+++ src/database/foo/foo.service.spec.ts
@@ -1,28 +1,13 @@
-import { MikroOrmModule } from "@mikro-orm/nestjs";
-import { Test, TestingModule } from "@nestjs/testing";
+import { MikroORM } from "@mikro-orm/core";
 import config from "../../mikro-orm.config";
-import { Bar } from "./entities/bar.entity";
-import { Foo } from "./entities/foo.entity";
-import { FooService } from "./foo.service";
 
 describe("Foo Service", () => {
-  let service: FooService;
-
+  let orm: MikroORM;
   beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        MikroOrmModule.forRoot({
-          ...config,
-          debug: ["query", "query-params", "info"],
-        }),
-        MikroOrmModule.forFeature([Foo, Bar]),
-      ],
-    }).compile();
-
-    service = module.get(FooService);
+    orm = await MikroORM.init(config);
   });
 
   it("should be defined", () => {
-    expect(service).toBeDefined();
+    expect(orm).toBeDefined();
   });
 });
